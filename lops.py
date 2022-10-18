f50cf7e3eb97c6985b3bb20814d7888a3c398c2a
agderegibus <agderegibus@gmail.com>
2022-10-18 12:34:32 -0300

    Primer armado de los archivos

--- lops.py
+++ lops.py
@@ -3,3 +3,12 @@ import requests
 import time 
 import random
 
+url_saldos_totales = "https://riskzone.anywhereportfolio.com.ar:9099/api/saldosconsolidados"
+
+tokenobj = {'key': 'value'}
+REQUEST_GENERAL = requests.get(url_saldos_totales, json = tokenobj)
+data  = (REQUEST_GENERAL.json())
+df = pd.DataFrame(data)
+
+
+df.to_excel("Exportacion_test.xls")
